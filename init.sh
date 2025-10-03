5f96a95ba0cfb8ff2953b54853cf69eec2c0e51a
dsgwzp <1132956063@qq.com>
2025-10-03 22:45:06 +0800

    feat(ci): 新增 NeZha 自动化部署工作流 为项目引入了完整的自动化部署流程。为支持此功能，同时进行了以下优化： - ci: 新增 NeZha.yml 工作流，以实现服务的自动化部署 - build: 优化 Build.yml，实施单一标签策略并启用缓存 - script: 改进 init.sh，使其支持从环境变量 $PORT 读取端口 (默认 80)

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ if [ ! -s /etc/supervisor/conf.d/damon.conf ]; then
   GRPC_PROXY_PORT=443
   GRPC_PORT=8008
   WEB_PORT=8080
-  PRO_PORT=${PRO_PORT:-'80'}
+  PRO_PORT=${PORT:-'80'}
   BACKUP_TIME=${BACKUP_TIME:-'0 4 * * *'}
   BACKUP_NUM= ${BACKUP_NUM:-'5'}
   CADDY_HTTP_PORT=2052
@@ -276,7 +276,7 @@ EOF
 
   if [[ "$DASHBOARD_VERSION" =~ 0\.[0-9]{1,2}\.[0-9]{1,2}$ ]]; then
     # 下载包含本地数据的 sqlite.db 文件，生成18位随机字符串用于本地 Token
-    wget -P ${WORK_DIR}/data/ ${GH_PROXY}https://github.com/Kiritocyz/Argo-Nezha-Service-Container/raw/main/sqlite.db
+    wget -P ${WORK_DIR}/data/ ${GH_PROXY}https://github.com/wzp7411/Argo-Nezha-Service-Container/raw/main/sqlite.db
     LOCAL_TOKEN=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 18)
     sqlite3 ${WORK_DIR}/data/sqlite.db "update servers set secret='${LOCAL_TOKEN}' where created_at='2023-04-23 13:02:00.770756566+08:00'"
   fi
@@ -354,7 +354,7 @@ DASHBOARD_VERSION=$DASHBOARD_VERSION
 EOF
 
   # 生成 backup.sh 文件的步骤2 - 在线获取 template/bakcup.sh 模板生成完整 backup.sh 文件
-  wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/Kiritocyz/Argo-Nezha-Service-Container/main/template/backup.sh | sed '1,/^########/d' >> $WORK_DIR/backup.sh
+  wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/wzp7411/Argo-Nezha-Service-Container/main/template/backup.sh | sed '1,/^########/d' >> $WORK_DIR/backup.sh
 
   if [[ -n "$GH_BACKUP_USER" && -n "$GH_EMAIL" && -n "$GH_REPO" && -n "$GH_PAT" ]]; then
     # 生成 restore.sh 文件的步骤1 - 设置环境变量
@@ -376,7 +376,7 @@ IS_DOCKER=1
 EOF
 
     # 生成 restore.sh 文件的步骤2 - 在线获取 template/restore.sh 模板生成完整 restore.sh 文件
-    wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/Kiritocyz/Argo-Nezha-Service-Container/main/template/restore.sh | sed '1,/^########/d' >> $WORK_DIR/restore.sh
+    wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/wzp7411/Argo-Nezha-Service-Container/main/template/restore.sh | sed '1,/^########/d' >> $WORK_DIR/restore.sh
   fi
 
   # 生成 renew.sh 文件的步骤1 - 设置环境变量
@@ -391,7 +391,7 @@ TEMP_DIR=/tmp/renew
 EOF
 
   # 生成 renew.sh 文件的步骤2 - 在线获取 template/renew.sh 模板生成完整 renew.sh 文件
-  wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/Kiritocyz/Argo-Nezha-Service-Container/main/template/renew.sh | sed '1,/^########/d' >> $WORK_DIR/renew.sh
+  wget -qO- ${GH_PROXY}https://raw.githubusercontent.com/wzp7411/Argo-Nezha-Service-Container/main/template/renew.sh | sed '1,/^########/d' >> $WORK_DIR/renew.sh
 
   # 生成定时任务: 1.每天北京时间 3:30:00 更新备份和还原文件，2.每天北京时间 4:00:00 备份一次，并重启 cron 服务； 3.每分钟自动检测在线备份文件里的内容
   [ -z "$NO_AUTO_RENEW" ] && [ -s $WORK_DIR/renew.sh ] && ! grep -q "$WORK_DIR/renew.sh" /etc/crontab && echo "30 3 * * * root bash $WORK_DIR/renew.sh" >> /etc/crontab
